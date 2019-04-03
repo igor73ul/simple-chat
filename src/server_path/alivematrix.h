11dc22fc667206c5ee31ff7ff2f3b3105c8ef259
Igor <not.real@mail.ru>
2019-04-03 22:30:51 +0400

    схема контроля за состоянием абонента расширена: анализируем какое сообщение пришло - регистрация, удаление или прочее.

--- src/server_path/alivematrix.h
+++ src/server_path/alivematrix.h
@@ -10,7 +10,9 @@ class AliveMatrix {
 public:
     AliveMatrix() = default;
     //обновить состояние-жив для абонента kUserId идентификатором
-    void update(const int kUserId){ matrix_[kUserId] = AliveState::kUpdateNow; }
+    void update(const int kUserId);
+    void del(const int kUserId) { matrix_[kUserId] = AliveState::kLost; }
+    void insert(const int kUserId) { matrix_[kUserId] = AliveState::kUpdateNow; }
     std::list<int> eraselist(); //Забрать кандидатов на удаление, завершив текущую итерацию обновлений
 private:
     enum class AliveState {
@@ -22,6 +24,12 @@ private:
     bool isAlive(AliveState) const noexcept; //оценить состояние (жив ли)
     std::map<int, AliveState> matrix_;
 };
+
+inline void AliveMatrix::update(const int kUserId) {
+    auto iter = matrix_.find(kUserId);
+    if(iter != matrix_.end())
+        iter->second = AliveState::kUpdateNow;
+}
 
 }
 #endif // ALIVEMATRIX_H
